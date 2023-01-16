2bfa55f50a3f0121ebe0313c1458e77733213928
Denorivan <vsevolod.krasnikov@ukr.net>
2023-01-16 18:33:00 +0200

    Final version

---
+++ src/test/java/ua/com/my/RunApplicationTests.java
@@ -0,0 +1,11 @@
+package ua.com.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
